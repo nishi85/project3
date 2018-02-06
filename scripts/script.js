96785b982bc8a1bdb4acddf81fb08dd74fb555e8
Nishi Rathod <nishi85@outlook.com>
2018-02-06 17:54:47 -0500

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+//user begins quiz by clicking on button
+
+//scroll down to begin quiz
+
+//first question is asked. Depending on answer, user will be given a certain set of questions
+
+//each answer is assigned a value
+
+//values are in a range that determine a set of podcasts
+
+//question catered towards the length preference will serve as a hard filter
+
+//user will click on button to calculate final score. Make sure user filled out all questions
+
+//final score determines which postcast will be displayed
+//podcast result will have link, pic and info
+
+//allow user to retake quiz
